8d16316b5500dacd4bfcc4c8f240527d62da35fa
romanchenko31 <romanchenko.zhen@gmail.com>
2025-02-22 23:17:58 +0100

    add_fix

--- src/app/components/ServiceCard.js
+++ src/app/components/ServiceCard.js
@@ -1,8 +1,16 @@
+import Image from "next/image";
+
 export default function ServiceCard({ title, description, icon }) {
   return (
     <div className="bg-white p-6 rounded-lg shadow-lg">
       <div className="text-center mb-4">
-        <img src={icon} alt={title} className="w-16 h-16 mx-auto" />
+        <Image
+          width={80}
+          height={80}
+          src={icon}
+          alt={title}
+          className="w-16 h-16 mx-auto"
+        />
       </div>
       <h3 className="text-xl font-semibold">{title}</h3>
       <p>{description}</p>
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,57 @@
 export default function Contact() {
   return (
-    <section className="container mx-auto p-4">
-      <h2 className="text-3xl font-semibold text-center mb-6">Contact</h2>
-      <span>Наші телефони:</span>
-      <a href="tel:+380501336304">+380501336304</a>
+    <section className="container mx-auto p-6 md:p-10 bg-gray-100 bg-opacity-40 rounded-lg shadow-lg text-center">
+      <h2 className="text-4xl font-bold text-blue-600 mb-6">Наші контакти</h2>
+
+      <div className="text-lg text-gray-800 mb-6">
+        <p className="font-semibold">Наші телефони:</p>
+        <a
+          href="tel:+380501336304"
+          className="block text-blue-500 hover:underline text-xl font-semibold"
+        >
+          +380501336304
+        </a>
+      </div>
+
+      <div className="text-lg text-gray-800 mb-6">
+        <p className="font-semibold">Електронна пошта:</p>
+        <a
+          href="mailto:voltag.dp@gmail.com"
+          className="block text-blue-500 hover:underline text-xl font-semibold"
+        >
+          voltag.dp@gmail.com
+        </a>
+      </div>
+
+      <div className="text-lg text-gray-800">
+        <p className="font-semibold">Ми в соцмережах:</p>
+        <div className="flex justify-center gap-4 mt-3">
+          <a
+            href="https://www.facebook.com/elektryk"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 text-2xl hover:text-blue-800"
+          >
+            <i className="fab fa-facebook"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/elektryk"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-pink-600 text-2xl hover:text-pink-800"
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a
+            href="https://www.youtube.com/c/elektryk"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-red-600 text-2xl hover:text-red-800"
+          >
+            <i className="fab fa-youtube"></i>
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
