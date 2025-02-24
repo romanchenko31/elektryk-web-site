066bb6f70e5c5bb195976070530e19dbb51b6ec0
romanchenko31 <romanchenko.zhen@gmail.com>
2025-02-24 21:32:13 +0100

    fix_ChatForm

--- src/app/components/ChatForm.js
+++ src/app/components/ChatForm.js
@@ -1,6 +1,7 @@
-"use client"; // Добавьте эту строку
+"use client"; // Директива для клиентского компонента
 
 import { useState } from "react";
+import styles from "./chatForm.module.css"; // Импорт стилей
 
 const ChatForm = () => {
   const [name, setName] = useState("");
@@ -13,20 +14,22 @@ const ChatForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Ваше имя"
-      />
-      <textarea
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        placeholder="Сообщение"
-      />
-      <button type="submit">Отправить</button>
-    </form>
+    <div className={styles.chatFormContainer}>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Ваше имя"
+        />
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Сообщение"
+        />
+        <button type="submit">Отправить</button>
+      </form>
+    </div>
   );
 };
 
